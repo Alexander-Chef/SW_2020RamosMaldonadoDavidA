8da4025f3db85a222553b163c2165158768ce2f3
Alexander-Chef <camposdavid047@gmail.com>
2020-05-14 13:37:55 -0500

    FEAT: Se agrego nombre y cosas que me gustaron del curso

---
+++ Actividad2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Actividad2</title>
+  </head>
+  <body>
+    <?php
+      echo "Ramos Maldonado David Alexander";
+      echo "<br>";
+      echo "Las cosas que me han gustado del curso son la posibilidad de aprender más y las oportunidades que me podría ofrecer.";
+    ?>
+  </body>
+</html>
